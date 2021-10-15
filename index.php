cffccaffb697f3d5b91db7a73d766c3827db1815
bourrrin <44211311+bourrrin@users.noreply.github.com>
2021-10-15 13:58:42 -0400

    test animation 3d

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="zoom_back">
+        <div id="zoom_book">
+            <img id="book" src="img/book.png" alt="">
+        </div>
+        <img class="background" src="img/taverne.jpg" alt="">
+    </div>
+</body>
+</html>
