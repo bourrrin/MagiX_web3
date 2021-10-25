14f74ebda811733dd750cf081a6955cfc6f0f0b3
bourrrin <44211311+bourrrin@users.noreply.github.com>
2021-10-25 19:53:31 -0400

    horreur v.0

--- index.php
+++ index.php
@@ -4,15 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/lucAPI.css">
     <title>Document</title>
 </head>
 <body>
-    <div id="zoom_back">
-        <div id="zoom_book">
-            <img id="book" src="img/book.png" alt="">
-        </div>
-        <img class="background" src="img/taverne.jpg" alt="">
+    <div id=background></div>
+    <div id=foreground></div>
+    <div class="page_title">
+        <h1>MAGIX</h1>
+        <h4>horror card game</h4>
     </div>
+    <form class=login action="" method="post">
+        <div class="flex_column">
+            <input type="text" name="usernemae" placeholder="Username">
+            <input type="text" name="mdp" placeholder="Password">
+            <button class="btn_no_style"> LOG IN</button>
+        </div>
+    </form>
+    <div id=zombie_hand></div>
+
 </body>
 </html>
