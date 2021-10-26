d85eea41293a450a442b5dd5bb695c099bf94521
bourrrin <stbarth.luca@gmail.com>
2021-10-26 13:59:36 -0400

    style input login

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     <img class="ship move-right" id="ship0" src="img/ships/ship0.png" alt="">
     <img class="ship move-left" id="ship1" src="img/ships/ship1.png" alt="">
     <img class="ship move-left" id="ship2" src="img/ships/ship2.png" alt="">
-    <img class="ship move-right" id="ship3" src="img/ships/ship3.png" alt="">  
-    <img class="ship move-left" id="ship4" src="img/ships/ship4.png" alt="">   
+    <img class="ship move-right" id="ship3" src="img/ships/ship3.png" alt="">
+    <img class="ship move-left" id="ship4" src="img/ships/ship4.png" alt="">
     <img class="ship move-right" id="ship5" src="img/ships/ship5.png" alt="">
     <img class="ship move-right" id="ship6" src="img/ships/ship6.png" alt="">
     <img class="ship move-left" id="ship7" src="img/ships/ship7.png" alt="">
@@ -23,9 +23,11 @@
         <h1>MAGIX</h1>
     </div>
     <form class=login action="" method="post">
-        <div class="flex_column">
-            <input type="text" name="usernemae" placeholder="Username">
-            <input type="text" name="mdp" placeholder="Password">
+        <div>
+            <div>
+                <input type="text" name="usernemae" placeholder="Username">
+                <input type="text" name="mdp" placeholder="Password">
+            </div>
             <button class="btn_no_style"> LOG IN</button>
         </div>
     </form>
