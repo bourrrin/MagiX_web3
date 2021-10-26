41d193d5d31422ca4ad079e403a5eac957511ddf
bourrrin <stbarth.luca@gmail.com>
2021-10-26 11:21:24 -0400

    MORE SHIPS!!!!

--- index.php
+++ index.php
@@ -9,7 +9,16 @@
     <title>Document</title>
 </head>
 <body>
-    <img class="ship" src="img/ship0.png" alt="">
+    <img class="ship move-right" id="ship0" src="img/ships/ship0.png" alt="">
+    <img class="ship move-left" id="ship1" src="img/ships/ship1.png" alt="">
+    <img class="ship move-left" id="ship2" src="img/ships/ship2.png" alt="">
+    <img class="ship move-right" id="ship3" src="img/ships/ship3.png" alt="">  
+    <img class="ship move-left" id="ship4" src="img/ships/ship4.png" alt="">   
+    <img class="ship move-right" id="ship5" src="img/ships/ship5.png" alt="">
+    <img class="ship move-right" id="ship6" src="img/ships/ship6.png" alt="">
+    <img class="ship move-left" id="ship7" src="img/ships/ship7.png" alt="">
+    <img class="ship move-right" id="ship8" src="img/ships/ship8.png" alt="">
+
     <div class="page_title">
         <h1>MAGIX</h1>
     </div>
