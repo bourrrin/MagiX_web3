7fe04f3266e73fc5839e5dcf86fd6b5bb9e6c019
bourrrin <stbarth.luca@gmail.com>
2021-11-05 18:28:22 -0400

    ANIMATION ENTREE LOBBY !!!!!!!!! DONE AND DUSTED

--- lobby.php
+++ lobby.php
@@ -3,7 +3,10 @@
 ?>
     <link rel="stylesheet" href="css/lobby.css">
     <script src="js/lobby.js"></script>
-
+    <div id="background_lobby"></div>
+    <div id="blurr"></div>
+    <div class="eye" id="eye-top"></div>
+    <!-- <div class="eye" id="eye-bottom"></div> -->
 
 <?php
     require_once("partial/footer.php");
