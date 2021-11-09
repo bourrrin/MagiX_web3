a442f3f18c455a4c5d947ffbe9406ca8c87e93a2
bourrrin <stbarth.luca@gmail.com>
2021-11-09 15:12:43 -0500

    changement de style pour lobby

--- lobby.php
+++ lobby.php
@@ -3,19 +3,13 @@
 ?>
     <link rel="stylesheet" href="css/lobby.css">
     <script src="js/lobby.js"></script>
-    <div id="background_lobby"></div>
-    <div class="darken-filter"></div>
+
     <div class=lobby-wrapper>
-        <div class="main-menu">
-            <img class="element_back" src="img/lobby/main_screen_back.jpg" alt="">
-            <div class="btn_menu">
-                <div class="element" id="jouer">Jouer</div>
-                <div class="element" id="pratique">Pratique</div>
-                <div class="element" id="quitter">Quitter</div>
-            </div>
-
-
-        </div>
+        <div class="element" id="jouer">Jouer</div>
+        <div class="element" id="pratique">Pratique</div>
+        <div class="element" id="quitter">Quitter</div>
+        <!-- <img src="img/lobby/giphy.gif" alt=""> -->
+        <!-- <img src="img/lobby/brain.gif" alt=""> -->
         <div class="chat">
             <iframe
             src="https://magix.apps-de-cours.com/server/#/chat/<?=  $_SESSION["player_data"]->key?>?v=1" onload="applyStyles(this)" >
